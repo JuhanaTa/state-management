d7241c024f8491edea3272df1002ccbe08909aba
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-04-07 16:35:21 +0300

    new passport branch

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 'use strict';
 const express = require('express');
 const session = require('express-session');
+const passport = require('./utils/pass');
 const app = express();
 const port = 3000;
+
+const loggedIn = (req, res, next) => {
+  console.log('logged', req.user);
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
 
 app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 app.use(express.json()); // for parsing application/json
@@ -16,7 +26,8 @@ app.use(session( {
   saveUninitialized: true,
 }));
 
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -36,15 +47,30 @@ app.get('/form', (req, res) => {
   res.render('form')
 });
 
-app.get('/secret', (req, res) => {
+/*app.get('/secret', (req, res) => {
   if (req.session.logged) {
     res.render('secret');
   } else {
     res.redirect('/form');
   }
+});*/
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
 });
 
-app.post('/login', (req, res) => {
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/form'}),
+    (req, res) => {
+      console.log('success');
+      res.redirect('/secret');
+    });
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+/*app.post('/login', (req, res) => {
   if (rightUserName === req.body.username && rightPassword === req.body.password) {
     req.session.logged = true;
     res.redirect('/secret');
@@ -52,9 +78,9 @@ app.post('/login', (req, res) => {
     req.session.logged = false;
     res.redirect('/form');
   }
-});
+});*/
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));999
